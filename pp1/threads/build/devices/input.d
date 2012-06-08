beafe9534f863057a8b23a1ada216f177ed4b845
manu zhang <owenzhang1990@gmail.com>
2012-06-08 10:18:07 +0800

    delete objects and denpendents

--- pp1/threads/build/devices/input.d
+++
@@ -1,5 +0,0 @@
-devices/input.o: ../../devices/input.c ../../devices/input.h \
- ../../lib/stdbool.h ../../lib/stdint.h ../../lib/debug.h \
- ../../devices/intq.h ../../threads/interrupt.h ../../threads/synch.h \
- ../../lib/kernel/list.h ../../lib/stddef.h \
- ../../lib/kernel/priority_queue.h ../../devices/serial.h
