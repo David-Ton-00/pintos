fa25719349d3f8109b3844fc0afbc7f71dda8a51
manu zhang <owenzhang1990@gmail.com>
2012-06-08 10:17:18 +0800

    add README

---
+++ pp1/threads/build/lib/kernel/priority_queue.d
@@ -0,0 +1,3 @@
+lib/kernel/priority_queue.o: ../../lib/kernel/priority_queue.c \
+ ../../lib/kernel/priority_queue.h ../../lib/stdbool.h ../../lib/stddef.h \
+ ../../lib/stdint.h ../../lib/kernel/../debug.h
---
+++ pp1/threads/build/threads/malloc.d
@@ -0,0 +1,7 @@
+threads/malloc.o: ../../threads/malloc.c ../../threads/malloc.h \
+ ../../lib/debug.h ../../lib/stddef.h ../../lib/kernel/list.h \
+ ../../lib/stdbool.h ../../lib/stdint.h ../../lib/round.h \
+ ../../lib/stdio.h ../../lib/stdarg.h ../../lib/kernel/stdio.h \
+ ../../lib/string.h ../../threads/palloc.h ../../threads/synch.h \
+ ../../lib/kernel/priority_queue.h ../../threads/vaddr.h \
+ ../../threads/loader.h
