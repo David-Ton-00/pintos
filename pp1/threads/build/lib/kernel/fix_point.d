53ec69ae0d52f4e1dc4f2f29e43a3f4447545c12
manu zhang <owenzhang1990@gmail.com>
2012-06-20 19:12:18 +0800

    clean up

--- pp1/threads/build/lib/kernel/fix_point.d
+++
@@ -1,2 +0,0 @@
-lib/kernel/fix_point.o: ../../lib/kernel/fix_point.c \
- ../../lib/kernel/fix_point.h ../../lib/stdint.h ../../lib/debug.h
--- pp1/threads/build/lib/kernel/priority_queue.d
+++
@@ -1,3 +0,0 @@
-lib/kernel/priority_queue.o: ../../lib/kernel/priority_queue.c \
- ../../lib/kernel/priority_queue.h ../../lib/stdbool.h ../../lib/stddef.h \
- ../../lib/stdint.h ../../lib/kernel/../debug.h
